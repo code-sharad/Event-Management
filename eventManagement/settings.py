0601c1cf13602ddf29c746dc508527c86e4f9a85
code-sharad <codesharad@gmail.com>
2025-02-01 09:10:51 +0530

    add allowed hosts for deployment configuration

--- eventManagement/settings.py
+++ eventManagement/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    'event-management-456128109301.asia-south1.run.app'
+]
