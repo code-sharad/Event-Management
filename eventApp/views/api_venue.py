58662e27ac2edd6f09b1745de253d3f537feb378
Sharad Bhadait <61672294+code-sharad@users.noreply.github.com>
2025-02-24 22:27:42 +0530

    Update api_venue.py

--- eventApp/views/api_venue.py
+++ eventApp/views/api_venue.py
@@ -3,19 +3,19 @@ from ..models.venue import Venue,VenueCreate,VenueOut,VenueFilter
 from typing import List
 venueRouter = Router()
 
-@venueRouter.post("/create", response=VenueOut)
+@venueRouter.post("/create", response=VenueOut,tags=["venue"],description="Create a new venue")
 def create_venue(request, payload: VenueCreate):
     venue = Venue.objects.create(**payload.dict())
     return venue
 
-@venueRouter.get('/list',response=List[VenueOut])
+@venueRouter.get('/list',response=List[VenueOut],tags=['venue'],description="List all venues")
 def get_venue_list(request):
     venue = Venue.objects.all();
     return venue;
 
 
 # Get a single venue
-@venueRouter.get("/id={venue_id}", response=VenueOut)
+@venueRouter.get("/id={venue_id}", response=VenueOut,tags=["venue"],description="Get a single venue")
 def get_venue(request, venue_id: int):
     venue = Venue.objects.get(id=venue_id)
     return venue
@@ -24,7 +24,7 @@ def get_venue(request, venue_id: int):
 # List venues with filtering
 
 # http://localhost:8000/api/venue/venues/?min_capacity=1000&amenities=WiFi 
-@venueRouter.get("/venues/", response=list[VenueOut])
+@venueRouter.get("/venues/", response=list[VenueOut],tags=["venue"],description="List venues with filtering")
 def list_venues(request, filters: VenueFilter = Query(...)):
     queryset = Venue.objects.all()
     if filters.name:
